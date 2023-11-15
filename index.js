3612edd5d5f15a4706da9ab5ff4c15ed74ccfa78
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-15 12:36:53 +0600

    differant module usages

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const { a, add } = require("./local-1");
+const { a: a2, add: add2 } = require("./local-2");
 
-console.log(add(1, 3));
+console.log(add2(1, 3, 3));
---
+++ local-2.js
@@ -0,0 +1,9 @@
+const add = (p1, p2, p3) => p1 + p2 + p3;
+const a = 20;
+
+module.exports = add;
+
+module.exports = {
+  a,
+  add,
+};
