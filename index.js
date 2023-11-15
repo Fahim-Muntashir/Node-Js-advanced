8256bfddab8ca101126969a9542a4ebbb74ea0d1
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-11-15 12:41:08 +0600

    path module

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+// local module
 const { a, add } = require("./local-1");
 const { a: a2, add: add2 } = require("./local-2");
 
 console.log(add2(1, 3, 3));
+
+// built in module
+
+const path = require("path");
+console.log(path.join("/E:/LearningNode/", "local-1.js"));
